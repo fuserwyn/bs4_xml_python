46bdf63d3c31abba5fa41deccfd3211dd1a0364b
Ivan Sishko <lofitravel@MacBook-Pro-Ivan.local>
2023-02-19 15:34:42 +0300

    corrections commit

--- parser.py
+++ parser.py
@@ -7,15 +7,7 @@ for tag in soup.find_all('atom:title'):
     if tag.text == 'Document_КартыЛПАБ_КатегорииРеакцийРаботника':
         xml_file = open(f'{tag.text}.xml', "w+")
         xml_file.close()
-# with open('input.xml', encoding="utf-8") as fd: 
-#     lines = fd.readlines()
-#     for line in lines:
-#         if line.strip()[:7] == "<entry>":
-#             with open('Document_КартыЛПАБ_КатегорииРеакцийРаботника.xml', "w", encoding="utf-8") as file:
-#                 file.write(line.strip() + '\n')
-#         if line.strip()[:8] == "</entry>":
-#             with open('Document_КартыЛПАБ_КатегорииРеакцийРаботника.xml', "a", encoding="utf-8") as file:
-#                 file.write(line.strip())
+
 with open('input.xml', 'r') as f:
 	file_from_read = f.read() 
 sp = BeautifulSoup(file_from_read, 'xml')
